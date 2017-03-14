9921b8fc42f8c1fa7dabc4fff67c9dff70fb23c6
Ricardo Garrido <ricgarridoc@hotmail.com>
2017-03-13 23:15:14 -0600

    while

---
+++ while.php
@@ -0,0 +1,19 @@
+<?php
+/*
+
+$x = 1;
+while ( $x <= 10) { //Dentro de los parentesis del while va una condición.
+	echo $x . "<br>"; //Las instrucciones se van a repetir mientras que la condición sea verdadera
+$x++;
+}
+*/
+
+$x = 20;
+while ($x >= 1) {
+	echo $x ."<br>";
+	$x--;
+}
+
+
+
+?>
