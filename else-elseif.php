3023fd892eb6ab252c1b1ccdb13a8302cf8dac99
Ricardo Garrido <ricgarridoc@hotmail.com>
2017-03-13 23:10:35 -0600

    Else-elseif

---
+++ else-elseif.php
@@ -0,0 +1,39 @@
+
+
+
+<?php
+/*
+$edad = 18;
+	if ($edad >= 18) {
+		echo "Bienvenido, eres mayor de edad";
+	} else ($edad < 18) {
+		echo "Lo siento, eres menor, no puedes pasar";
+	}
+
+if (condition) { //aquí preguntas una condicion y si se cumple pasas a la otra condicion
+	if (condition) {//A esto se le llama anidar condicionales, sirven para preguntar una cosa y,
+		//si se cumples pasas a la otra.
+		
+	}
+}
+*/
+
+$mes ='Septiembre';
+if ($mes == "Diciembre") {
+	echo "Feliz navidad";
+} elseif ($mes == "Enero") {
+	echo "Feliz año nuevo";
+} elseif ($mes == "Septiembre") {
+	echo "Feliz mes patrio";
+}
+
+/*
+$mes = 'Enero';
+$verdadero = 'true';
+if ($verdadero){
+	echo "Verdadero";
+} else {
+	echo "Falso";
+}
+*/
+?>
